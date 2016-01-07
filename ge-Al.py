600ef40338ca5abad3cf73c87105909d01e0961f
soccjs <junseuk1202@gmail.com>
2016-01-07 11:28:49 +0900

    start example

---
+++ ge-Al.py
@@ -0,0 +1,11 @@
+print "soccjs"
+print "hi ge-algorithm"
+
+tag_set=["friend","airport","birthday","graduation",
+         "mom","university","cake","family","trip",
+         "vacation","school","food","coffee","flight",
+         "interview","restaurant"
+         ]
+
+#print tag_set
+print len(tag_set)
