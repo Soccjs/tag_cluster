151054740c5f3639034e62d8103b014b07752857
soccjs <junseuk1202@gmail.com>
2016-01-08 03:15:28 +0900

    test random put, not working perl module

--- ge-Al.py
+++ ge-Al.py
@@ -1,3 +1,6 @@
+import random
+import Queue
+
 print "soccjs"
 print "hi ge-algorithm"
 
@@ -8,4 +11,30 @@ tag_set=["friend","airport","birthday","graduation",
          ]
 
 #print tag_set
-print len(tag_set)
+total_len = len(tag_set)
+print total_len 
+
+q = Queue.Queue()
+q.put(tag_set)
+
+while True:
+    C = q.get()
+    A = Queue.Queue()
+    B = Queue.Queue()
+    
+    split_n = random.randint(1, total_len-1)
+    print "split_n" , split_n
+
+    for i in range(0,split_n):
+        word = random.choice(C)
+        C.remove(word)
+        A.put(word)
+    for i in range(0,total_len-split_n):
+        word = random.choice(C)
+        C.remove(word)
+        B.put(word)
+        
+    
+    
+    print A.qsize(), B.qsize(), len(C)
+    break    
---
+++ perl-test.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append('~/Documents/env/pyperl-1.0')
+#from perlmodule import perl
+print "perl-test"
+
+six = perl.eval("3+3");
+print six
+
+#perl.eval("use WordNet::Similarity::jcn")
