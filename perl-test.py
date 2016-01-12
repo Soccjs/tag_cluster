e0e79203db66f50311e356dc819fd81ddb96dca2
soccjs <junseuk1202@gmail.com>
2016-01-12 17:46:40 +0900

    how to use wordnet in python

--- perl-test.py
+++ perl-test.py
@@ -1,9 +1,25 @@
-import sys
-sys.path.append('~/Documents/env/pyperl-1.0')
-#from perlmodule import perl
-print "perl-test"
+#example how to use Wordnet in python
 
-six = perl.eval("3+3");
-print six
+from nltk.corpus import wordnet as wn
 
-#perl.eval("use WordNet::Similarity::jcn")
+#nltk.download() #install wordnet,wordnet_ic
+
+print 1,wn.synsets('dog'),"\n"
+
+print 2,"verb" ,"\n",  wn.synsets('dog', pos=wn.VERB),"\n"
+
+print 3,"noun" ,"\n", wn.synsets('dog', pos=wn.NOUN),"\n"
+
+print 4,wn.synset('dog.n.01'),"\n"
+
+print 5,"defination" , (wn.synset('dog.n.01').definition()),"\n"
+
+print 6,"example count" , len(wn.synset('dog.n.01').examples()),"\n"
+print 7,wn.synset('dog.n.01').examples()[0],"\n"
+
+dog = wn.synset('dog.n.01')
+print 8,dog.hypernyms(),"\n"
+
+print 9,"dog's root","\n",dog.root_hypernyms(),"\n"
+
+
